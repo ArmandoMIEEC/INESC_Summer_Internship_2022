5f123b34c033bcbdd552049449ccb8a4b080efb4
Armando <zearmando2000@gmail.com>
2022-07-26 13:17:41 +0100

    changes

--- graphs.m
+++ graphs.m
@@ -19,3 +19,19 @@ grid();
 xlabel('Frequência (GHz)');
 ylabel('Lado da amostra (cm)');
 set(gca, 'FontSize', 20);
+
+figure
+plot(ant_2233(:, 1) ./ 10^9, ant_2233(:, 2) .* 100, 'LineWidth', 2);
+title('Antena 22-33GHz', 'FontSize', 26);
+grid();
+xlabel('Frequência (GHz)');
+ylabel('Distânica até à amostra (cm)');
+set(gca, 'FontSize', 20);
+
+figure
+plot(ant_218(:, 1) ./ 10^9, ant_218(:, 2), 'LineWidth', 2);
+title('Antena 2-18GHz', 'FontSize', 26);
+grid();
+xlabel('Frequência (GHz)');
+ylabel('Distânica até à amostra (m)');
+set(gca, 'FontSize', 20);
